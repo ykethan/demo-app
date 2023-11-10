9f73c9bddc9b860322fa61cc437d145105f020a3
ykethan <ykethan@amazon.com>
2023-11-10 16:44:57 -0500

    post page effects

--- src/pages/postsPage.tsx
+++ src/pages/postsPage.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import * as Auth from "@aws-amplify/auth";
 import { Hub } from "aws-amplify/utils";
 import { PostCreateForm } from "@/ui-components";
+import { motion, AnimatePresence } from "framer-motion";
 
 import "@aws-amplify/ui-react/styles.css";
 import { PostCreateFormOverridesProps } from "@/ui-components/PostCreateForm";
@@ -115,6 +116,32 @@ export default function PostsPage() {
     },
   };
 
+  const postVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const buttonVariants = {
+    hover: { scale: 1.05 },
+    tap: { scale: 0.95 },
+  };
+
+  const formVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 100 },
+  };
+
+  const postItemVariants = {
+    hover: { scale: 1.03 },
+    tap: { scale: 0.97 },
+  };
+
+  const logoutButtonVariants = {
+    hover: { scale: 1.05, backgroundColor: "#ff0000" }, // Change color on hover
+    tap: { scale: 0.95 },
+  };
+
   return (
     <div>
       <nav className="bg-white shadow-md">
@@ -124,23 +151,35 @@ export default function PostsPage() {
               Profile
             </Link>
           </div>
-          <button
+          <motion.button
             onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-red-500 text-white font-bold py-2 px-4 rounded"
+            variants={logoutButtonVariants}
+            whileHover="hover"
+            whileTap="tap"
           >
             Logout
-          </button>
+          </motion.button>
         </div>
       </nav>
 
       <div className="container mx-auto p-4 bg-gray-100 min-h-screen">
         <main>
-          <div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={postVariants}
+            transition={{ staggerChildren: 0.1 }}
+          >
             {posts.map((post) => (
-              <article
+              <motion.article
                 key={post.id}
                 className="bg-white shadow-md rounded p-4 mb-4 cursor-pointer"
-                onClick={() => navigateToPostDetails(post.id)} // Add this line
+                onClick={() => navigateToPostDetails(post.id)}
+                variants={postItemVariants}
+                whileHover="hover"
+                whileTap="tap"
+                layout
               >
                 <h2 className="text-xl font-semibold text-gray-800">
                   {post.title}
@@ -149,25 +188,36 @@ export default function PostsPage() {
                 <small className="text-gray-500">
                   By {post.author || "Unknown author"}
                 </small>
-              </article>
+              </motion.article>
             ))}
-          </div>
+          </motion.div>
           {user && (
             <>
-              <button
+              <motion.button
                 onClick={toggleCreateForm}
                 className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+                variants={buttonVariants}
+                whileHover="hover"
+                whileTap="tap"
               >
                 {showCreateForm ? "Close Create Post" : "Create New Post"}
-              </button>
-              {showCreateForm && (
-                <div className="mt-4">
-                  <PostCreateForm
-                    onSubmit={handleSubmit}
-                    overrides={formOverrides}
-                  />
-                </div>
-              )}
+              </motion.button>
+              <AnimatePresence>
+                {showCreateForm && (
+                  <motion.div
+                    className="mt-4"
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={formVariants}
+                  >
+                    <PostCreateForm
+                      onSubmit={handleSubmit}
+                      overrides={formOverrides}
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </>
           )}
         </main>
